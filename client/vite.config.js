c5c6233ce0e32b470d957163cea0c0a4760290b1
brian boudrioux <brian.boudrioux@wildcodeschool.com>
2024-04-29 11:58:10 +0200

    update package

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/harmonia/",
+  base: "./",
   server: {
     port: 3000,
   },
